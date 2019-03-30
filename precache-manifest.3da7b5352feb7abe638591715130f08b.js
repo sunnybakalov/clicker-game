f95f8bcebfc937a729859e5ed0763fd22f37e776
Stan Bakalov <sunny.bakalov@gmail.com>
2019-03-30 12:24:10 -0600

    Updates

---
+++ precache-manifest.3da7b5352feb7abe638591715130f08b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd401eb4e9e99ab75dd2",
+    "url": "/clicker-game/static/js/runtime~main.8efb6c9b.js"
+  },
+  {
+    "revision": "0c8b29d71b899d27ceb9",
+    "url": "/clicker-game/static/js/main.253304e7.chunk.js"
+  },
+  {
+    "revision": "f05a1df3217379e807f2",
+    "url": "/clicker-game/static/js/2.fba19b92.chunk.js"
+  },
+  {
+    "revision": "0c8b29d71b899d27ceb9",
+    "url": "/clicker-game/static/css/main.a8f6abc5.chunk.css"
+  },
+  {
+    "revision": "8da95270b4ef6c6b00bf78200133075d",
+    "url": "/clicker-game/index.html"
+  }
+];
